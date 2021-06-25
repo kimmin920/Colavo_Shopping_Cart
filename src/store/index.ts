d3b38ed4e55803fb9d3b8522f8df755c1af60100
minwooKim <vvee1253@gmail.com>
2021-06-25 17:10:33 +0900

    Add: selectCart

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from './cartSlice';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     cart: cartSlice,
   },
 });
+
+type RootState = ReturnType<typeof store.getState>;
+
+export const selectCart = (state: RootState) => state.cart.items;
+
+export default store;
