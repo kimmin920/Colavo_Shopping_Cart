50f03e9dfbf98ef77a39d73640fb96bffeaf91f1
minwooKim <vvee1253@gmail.com>
2021-06-25 17:01:37 +0900

    Add: cartSlice & cartReducer

---
+++ src/store/cartSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface Item {
+  id: number;
+  count: number;
+  name: string;
+  price: number;
+};
+
+interface CartSliceState {
+  items: Item[];
+};
+
+const initialState: CartSliceState = {
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem: (state, action: PayloadAction<Item>) => {
+      state.items = [
+        ...state.items,
+        {
+          ...action.payload,
+          // NOTE: it's overriding id
+          id: state.items.length,
+        },
+      ];
+    },
+    removeItem: (state, action: PayloadAction<number>) => {
+        state.items = state.items.filter(({ id }) => id !== action.payload );
+    },
+  },
+});
+
+export const { addItem, removeItem } = cartSlice.actions;
+
+export default cartSlice.reducer;
