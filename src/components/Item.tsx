65ea9a8f342fb34c2a98b78d8dee745a747ea624
minwooKim <vvee1253@gmail.com>
2021-06-26 12:10:24 +0900

    Feat: getItems of salon

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,9 +1,22 @@
-import { useDispatch } from 'react-redux'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { selectSalon } from '../store';
 import { addItem, removeItem } from '../store/cartSlice';
+import { fetchSalonData } from '../store/salonSlice';
 
 export default function Item() {
   const dispatch = useDispatch();
+  const items = useSelector(selectSalon);
 
+  useEffect(() => {
+    async function getItems() {
+      await dispatch(fetchSalonData());
+    }
+
+    getItems();
+  }, [dispatch]);
+
+  console.log(items);
   return (
     <div>
       <button
