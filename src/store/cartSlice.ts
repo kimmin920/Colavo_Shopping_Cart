b42ced4580f69ea7478cd83a16bc9fd69a3698eb
minwooKim <vvee1253@gmail.com>
2021-06-27 13:59:59 +0900

    Add: discount reducers

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,20 +1,32 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import calculateTotal from '../utils/calculateTotalPrice';
+import { calculateTotalPrice, calculateItemPrice, calculateTotalDiscounts } from '../utils/calculatePrice';
 
 interface Item {
   id: string;
   count: number;
   name: string;
   price: number;
+  totalPrice?: number;
+  discount?: number;
+};
+
+type Discount = {
+  id: string,
+  name: string,
+  rate: number,
+  appliedItemIds?: string[],
+  discountPrice?: number,
 };
 
 interface CartSliceState {
   items: Item[];
+  discounts: Discount[];
   totalPrice: number;
 };
 
 const initialState: CartSliceState = {
   items: [],
+  discounts: [],
   totalPrice: 0,
 };
 
@@ -28,27 +40,64 @@ export const cartSlice = createSlice({
       }
 
       state.items.push(action.payload);
-      state.totalPrice = calculateTotal(state.items);
+      state.totalPrice = calculateTotalPrice(state.items);
+      // TODO: if there's discounts, need to push id to them.
+
+      if (state.discounts.length > 0) {
+        state.discounts.forEach((discount) => {
+          discount.appliedItemIds?.push(action.payload.id);
+        });
+      }
     },
     removeItem: (state, action: PayloadAction<string>) => {
       state.items = state.items.filter(({ id }) => id !== action.payload);
-      state.totalPrice = calculateTotal(state.items);
+      state.totalPrice = calculateTotalPrice(state.items);
     },
     increaseItemCount: (state, action: PayloadAction<{ id: string, count: number }>) => {
       const targetIndex = state.items.findIndex(item => item.id === action.payload.id);
+      const targetItem = state.items[targetIndex];
 
-      state.items[targetIndex].count += action.payload.count;
-      state.totalPrice = calculateTotal(state.items);
+      targetItem.count += action.payload.count;
+      targetItem.totalPrice = calculateItemPrice(targetItem);
+
+      state.totalPrice = calculateTotalPrice(state.items);
     },
     decreaceItemCount: (state, action: PayloadAction<{ id: string, count: number }>) => {
       const targetIndex = state.items.findIndex(item => item.id === action.payload.id);
+      const targetItem = state.items[targetIndex];
 
-      state.items[targetIndex].count -= action.payload.count;
-      state.totalPrice = calculateTotal(state.items);
+      targetItem.count -= action.payload.count;
+      targetItem.totalPrice = calculateItemPrice(targetItem);
+
+      state.totalPrice = calculateTotalPrice(state.items);
     },
-    addDiscount: () => {},
+    addDiscount: (state, action: PayloadAction<Discount>) => {
+      const allItemIdsInCart = Object.values(state.items).map(item => item.id);
+
+      if (allItemIdsInCart.length === 0) {
+        // NOTE: no items// add handler
+        return;
+      }
+
+      const newDiscount = {
+        ...action.payload,
+        appliedItemIds: allItemIdsInCart,
+      };
+
+      if (state.discounts.some(discount => discount.id === newDiscount.id)) {
+        return;
+      }
+
+      state.discounts.push(newDiscount);
+    },
     removeDiscount: () => {},
     clearCart: () => {},
+    updateTotalPrice: (state) => {
+      const totalItemPrice: number = calculateTotalPrice(state.items);
+      const totalDiscount: number = calculateTotalDiscounts(state.discounts, state.items);
+
+      state.totalPrice = totalItemPrice - totalDiscount;
+    },
   },
 });
 
@@ -57,6 +106,8 @@ export const {
   removeItem,
   increaseItemCount,
   decreaceItemCount,
+  addDiscount,
+  updateTotalPrice,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
