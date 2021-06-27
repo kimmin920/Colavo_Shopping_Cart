bc3cc6e5d94a0fab83703cc581a4e9d544d66614
minwooKim <vvee1253@gmail.com>
2021-06-27 22:35:13 +0900

    Feat: nav buttons and routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import styled, { css } from 'styled-components';
+import {
+  Route,
+  Switch,
+  Link,
+  BrowserRouter as Router,
+  Redirect,
+} from 'react-router-dom';
 import Cart from './Cart';
 import Item from './Item';
+import Discount from './Discount';
 import { fetchSalonData } from '../store/salonSlice';
 
 function App() {
@@ -16,11 +25,40 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <Cart />
-      <Item />
-    </>
+    <AppContainer>
+      <Router>
+        <Link to='/items'>+ 시술</Link>
+        <Link to='/discount'>- 할인</Link>
+        <Link to='/'>카트</Link>
+        <Switch>
+          <Route exact path='/' component={Cart} />
+          <Route exact path='/items' component={Item} />
+          <Route exact path='/discount' component={Discount} />
+          <Redirect to='/' />
+        </Switch>
+      </Router>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const ColumnBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const AppContainer = styled.div`
+  width: 400px;
+  height: 600px;
+  background: white;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 0.3rem;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+
+  & > div {
+    ${({ theme }) => css`
+      animation: ${theme.keyframes.fadeInUp} .5s ease-in-out;
+    `};
+  }
+`;
