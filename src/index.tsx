ddbcc20769b86ab7e976bc2d5a4d9f9073d228ae
minwooKim <vvee1253@gmail.com>
2021-06-28 01:22:07 +0900

    Add: themeProvider & GlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import App from './components/App';
 import store from './store';
+import GlobalStyle from './styles/globalStyle';
+import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
