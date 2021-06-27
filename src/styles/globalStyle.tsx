e883b3abbde5f5d6fb1efdeefe742435a8770a63
minwooKim <vvee1253@gmail.com>
2021-06-27 19:38:17 +0900

    Feat: fadeInUp & heartbeat animation

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -1,13 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  html, body {
+    height: 100vh;
+    width: 100vw;
+  }
+
   * {
     margin: 0;
     padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
   body {
     box-sizing: border-box;
+    background-color: #f6f6f6;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,5 @@
+import { fadeInUp, heartBeat } from './keyframes';
+
 const size = {
   mobile: '600px',
   tablet: '900px',
@@ -6,11 +8,22 @@ const size = {
 };
 
 const theme = {
-  mainColor: '#0a4297',
+  purple: '#9885f0',
+  lightPurple: '#ae9df1',
+  pink: '#ffa3c5',
+  lightPink: '#fdf0f5',
+  grey: '#96a0a9',
+  lightGrey: '#f7f7f7',
+  black: '#2a292b',
+  boxShadow: '0px 0px 11px 1px rgba(0,0,0,0.3)',
   mobile: `(max-width: ${size.mobile})`,
   tablet: `(max-width: ${size.tablet})`,
   laptop: `(max-width: ${size.laptop})`,
   desktop: `(min-width: ${size.desktop})`,
+  keyframes: {
+    fadeInUp,
+    heartBeat,
+  }
 };
 
 export default theme;
