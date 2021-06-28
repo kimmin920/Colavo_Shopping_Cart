bd73e4b9ef5c93b2ca9f0cb18fa43a46fd58a305
minwooKim <vvee1253@gmail.com>
2021-06-29 04:53:03 +0900

    Install: eslint and config

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'react/react-in-jsx-scope': 'off',
+    'indent': ['error', 2, { SwitchCase: 1, MemberExpression: 1 }],
+    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+    'no-plusplus': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-unused-vars': ['warn', { argsIgnorePattern: 'err|req|res|next' }],
+    'func-names': 'off',
+    'consistent-return': 'off',
+    'no-underscore-dangle': 'off',
+    'object-curly-spacing': ['error', 'always'],
+    'no-restricted-syntax': ['error', 'ForOfStatement'],
+    'no-use-before-define': ['error', { functions: false }],
+    'no-await-in-loop': 'off',
+    'object-shorthand': ['error', 'properties'],
+    'no-param-reassign': ['error', { props: false }],
+    'arrow-body-style': ['warn', 'as-needed'],
+    'wrap-iife': ['error', 'inside'],
+    'no-unused-expressions': 'off',
+  }
+};
