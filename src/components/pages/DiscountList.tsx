5b0f5584ccdea5316bdaae606d5ce3768b2ecc4a
minwooKim <vvee1253@gmail.com>
2021-06-28 17:31:25 +0900

    Move: discountList to pages

---
+++ src/components/pages/DiscountList.tsx
@@ -0,0 +1,42 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectSalonDiscounts } from '../../store';
+import { addDiscount } from '../../store/cartSlice';
+import {
+  StyledList,
+  StyledListItem,
+  StyledItemTitle,
+  StyledItemDiscount,
+} from '../../styles/styledListItem';
+import NavHeader from '../shared/NavHeader';
+
+export default function Discount(): JSX.Element {
+  const dispatch = useDispatch();
+  const discounts = useSelector(selectSalonDiscounts);
+  const discountsArray = Object.values(discounts);
+
+  return (
+    <>
+      <NavHeader title='할인' />
+      <StyledList>
+        {discountsArray.map(discount => (
+          <StyledListItem key={discount.id}>
+            <div>
+              <StyledItemTitle>
+                {discount.name}
+              </StyledItemTitle>
+              <StyledItemDiscount>
+                {discount.rate}
+              </StyledItemDiscount>
+            </div>
+            <button
+              onClick={() => dispatch(addDiscount(discount))}
+            >
+              select
+            </button>
+          </StyledListItem>
+        ))}
+      </StyledList>
+    </>
+  );
+}
+
