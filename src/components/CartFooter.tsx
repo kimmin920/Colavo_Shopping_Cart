f592fe8644089f58414f1267c6fae2df79a8e7b8
minwooKim <vvee1253@gmail.com>
2021-06-28 17:31:42 +0900

    Feat: cartFooter

---
+++ src/components/CartFooter.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { StyledLink } from '../styles/styledLink';
+
+type CartFooterProps = {
+  totalPrice: number,
+};
+
+export default function CartFooter({ totalPrice }: CartFooterProps) {
+  return (
+    <footer>
+      <TotalPriceWrapper>
+        합계
+        <TotalPrice>
+          {totalPrice}
+        </TotalPrice>
+      </TotalPriceWrapper>
+      <StyledLinkPurple to='/'>
+        결제
+      </StyledLinkPurple>
+    </footer>
+  );
+}
+
+const TotalPriceWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const TotalPrice = styled.div`
+  font-size: 2rem;
+`;
+
+const StyledLinkPurple = styled(StyledLink)`
+  background-color: ${({ theme }) => theme.purple};
+  width: 100%;
+  color: white;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.lightPurple};
+  }
+`;
