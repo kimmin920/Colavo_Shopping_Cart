27775f747a3c13485ff1101d6294f79b58c257b9
minwooKim <vvee1253@gmail.com>
2021-06-29 09:30:13 +0900

    Feat: to check if discount's already in cart

--- src/components/pages/DiscountList.tsx
+++ src/components/pages/DiscountList.tsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { selectSalonDiscounts } from '../../store';
+import { selectCartdiscounts, selectSalonDiscounts } from '../../store';
 import { addDiscount } from '../../store/cartSlice';
 import {
   StyledList,
@@ -9,33 +9,44 @@ import {
 } from '../../styles/styledListItem';
 import getPercentage from '../../utils/getPercentage';
 import NavHeader from '../shared/NavHeader';
+import { CheckIconPurple } from '../../styles/styledIcons';
+import Styledbutton from '../shared/Styledbutton';
+import Loader from '../shared/Loader';
 
 export default function Discount(): JSX.Element {
   const dispatch = useDispatch();
   const discounts = useSelector(selectSalonDiscounts);
   const discountsArray = Object.values(discounts);
+  const cartDiscounts = useSelector(selectCartdiscounts);
+
+  function isInCart(discountId: string) {
+    return cartDiscounts.find((cartDiscount) => cartDiscount.id === discountId);
+  }
 
   return (
     <>
       <NavHeader title='할인' />
       <StyledList>
-        {discountsArray.map(discount => (
-          <StyledListItem key={discount.id}>
-            <div>
-              <StyledItemTitle>
-                {discount.name}
-              </StyledItemTitle>
-              <StyledItemDiscount>
-                {getPercentage(discount.rate)}%
-              </StyledItemDiscount>
-            </div>
-            <button
-              onClick={() => dispatch(addDiscount(discount))}
-            >
-              select
-            </button>
-          </StyledListItem>
-        ))}
+        {discountsArray.length > 0
+          ? discountsArray.map(discount => (
+            <StyledListItem key={discount.id}>
+              <div>
+                <StyledItemTitle>
+                  {discount.name}
+                </StyledItemTitle>
+                <StyledItemDiscount>
+                  {getPercentage(discount.rate)}%
+                </StyledItemDiscount>
+              </div>
+              {isInCart(discount.id)
+                ? <CheckIconPurple />
+                : (
+                  <Styledbutton
+                    onClick={() => dispatch(addDiscount(discount))}
+                    text='적용'
+                  />)}
+            </StyledListItem>))
+          : <Loader />}
       </StyledList>
     </>
   );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { CartItem, CartSliceState } from '../types/cart.types';
+import { CartDiscount, CartItem, CartSliceState } from '../types/cart.types';
 import { CurrencyCode } from '../types/global.types';
 import { Discounts, Items } from '../types/salon.types';
 import cartSlice from './cartSlice';
@@ -16,6 +16,7 @@ type RootState = ReturnType<typeof store.getState>;
 
 export const selectCart = (state: RootState): CartSliceState => state.cart;
 export const selectCartItems = (state: RootState): CartItem[] => state.cart.items;
+export const selectCartdiscounts = (state: RootState): CartDiscount[] => state.cart.discounts;
 
 export const selectSalonItems = (state: RootState): Items => state.salon.data.items;
 export const selectSalonDiscounts = (state: RootState): Discounts => state.salon.data.discounts;
